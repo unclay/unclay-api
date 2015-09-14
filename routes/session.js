b1e6d4417b495667c644a57d48ed2257d42ff69b
wangcl <wcl58981298@gmail.com>
2015-09-14 21:43:03 +0800

    test

--- routes/session.js
+++ routes/session.js
@@ -7,10 +7,12 @@ var key = "__clid";
 var EXPIRES = 24 * 60 * 60 * 1000;
 
 var setEnd = function(req, res, next, session){
+	console.log("session20");
 	var host = req.headers.host.split(".");
 	host = "."+host[host.length-2]+"."+host[host.length-1];
 	req.session = session;
 	res.setHeader('Set-Cookie', key+'='+req.session.id+';domain='+host+';path=/;Expires='+new Date(req.session.cookie.expire).toGMTString()+';httpOnly=true');
+	console.log("session19");
 	next();
 }
 
@@ -26,29 +28,38 @@ var generate = function(req, res, next, sessionid){
 		var p = Model.Session.findOne({
 			__clid: session.id
 		}).exec();
+		console.log("session10");
 		p.then(function(doc, err){
+			console.log("session11", doc, err);
 			if( doc ){
+				console.log("session12");
 				session = doc.value;
 				session.cookie.expire = new Date().getTime() + EXPIRES;
 				sessions[session.id] = session;
                 return setEnd(req, res, next, session);
 			} else {
+				console.log("session13");
 				session.id = new Date().getTime() + Math.random() + "";
             	return new Model.Session({
             		__clid: session.id,
             		value: session
             	}).save(function(err, doc){
+            		console.log("session14", err, doc);
 					if (doc) {
+						console.log("session17");
 		                sessions[session.id] = session;
 		            	setEnd(req, res, next, session);
 		            } else {
+		            	console.log("session18");
 		            	dbthen();
 		            }
 				});
 			}
+			console.log("session16");
 		})
 		.then(null, dbthen);
 	}, function(err){
+		console.log("session15");
 		next();
 	});
 }
